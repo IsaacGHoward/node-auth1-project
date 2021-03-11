ec4004ed52d5cddb41d2196446e275fbfbfd21ab
IsaacGHoward <howardisaac100@gmail.com>
2021-03-10 17:59:50 -0800

    Implemented findById and add

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -20,14 +20,29 @@ function findBy(filter) {
   resolves to the user { user_id, username } with the given user_id
  */
 function findById(user_id) {
-
+  return db('users')
+    .select('user_id', 'username')
+    .where('user_id', user_id)
 }
 
 /**
   resolves to the newly inserted user { user_id, username }
  */
 function add(user) {
-
+  db('users')
+    .insert(user)
+    .then(ids => {
+      findById(ids[0])
+      .then((res) => {return res})
+    });
 }
+
+module.exports = {
+  find,
+  findBy,
+  findById,
+  add,
+}
+
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
